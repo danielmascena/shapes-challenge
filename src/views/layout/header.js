76114c352d8aec14ed7404cf4c3216297964bb3a
Daniel Mascena <danielmascena@gmail.com>
2020-01-13 08:50:17 -0300

    passing the modal from the layout to the header

--- src/views/layout/header.js
+++ src/views/layout/header.js
@@ -1,36 +1,24 @@
-import React, { useEffect, useRef } from "react";
-import dialogPolyfill from "dialog-polyfill";
+import React from "react";
 import "./header.css";
 
-const Header = () => {
-  const modalRef = useRef(null);
-  useEffect(() => {
-    //var dialog = document.querySelector("dialog");
-    dialogPolyfill.registerDialog(modalRef.current);
-  }, []);
-  return (
-    <header>
-      <h1 className="brand-title">
-        <span className="brand-title__text">Shapes</span>
-      </h1>
-      <nav>
-        <a
-          onClick={event => {
-            modalRef.current.showModal();
-            event.preventDefault();
-          }}
-        
-        >
-          About
-        </a>
-      </nav>
-      <dialog id="about" ref={modalRef}>
-        <h3 className="brand-modal">Shapes</h3>
-        <p>lorem</p>
-        <button onClick={()=>modalRef.current.close()}>Close</button>
-      </dialog>
-    </header>
-  );
-};
+const Header = ({modalRef}) => (
+  <header>
+    <h1 className="brand-title">
+      <span className="brand-title__text">Shapes</span>
+    </h1>
+    <nav>
+      <a
+        onClick={event => {
+          modalRef.current.showModal();
+          event.preventDefault();
+        }}
+      
+      >
+        About
+      </a>
+    </nav>
+    
+  </header>
+);
 
 export default Header;
--- src/views/layout/layout.js
+++ src/views/layout/layout.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import dialogPolyfill from "dialog-polyfill";
 import Header from "./header";
 import Footer from "./footer";
 
-const Layout = ({ children }) => (
-  <>
-    <Header />
-    {children}
-    <Footer />
-  </>
-);
+const Layout = ({ children }) => {
+  const modalRef = useRef(null);
+  useEffect(() => {
+    //var dialog = document.querySelector("dialog");
+    dialogPolyfill.registerDialog(modalRef.current);
+  }, []);
+  return (
+    <>
+      <Header {...{modalRef}} />
+      {children}
+      <dialog id="about" ref={modalRef}>
+        <h3 className="modal-heading"><span className="modal-heading__text">Shapes</span></h3>
+        <p>lorem</p>
+        <button onClick={()=>modalRef.current.close()}>Close</button>
+      </dialog>
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
