508a3c00bfc374b8f73e31eea5ea6f3bab2663a0
Daniel Mascena <daniel.mascena@dataprev.gov.br>
2020-01-22 18:34:44 -0300

    first release

---
+++ build/precache-manifest.e3d92ce2a6ca0f0ba6312bfd06c0ddb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5ff0ddb70514779016e",
+    "url": "/static/css/main.24dab2b6.chunk.css"
+  },
+  {
+    "revision": "f5ff0ddb70514779016e",
+    "url": "/static/js/main.f5ff0ddb.chunk.js"
+  },
+  {
+    "revision": "fdead53bc7b8ec8a76e9",
+    "url": "/static/js/1.fdead53b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d6f95a0db25d4fc5fb23bf1767b9f423",
+    "url": "/index.html"
+  }
+];
